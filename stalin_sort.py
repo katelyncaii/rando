57ed2a5c0fcf765a7f755a7dc74a631674336d3f
katelyncaii <166571058+katelyncaii@users.noreply.github.com>
2024-12-20 10:15:07 -0600

    better return

--- stalin_sort.py
+++ stalin_sort.py
@@ -9,21 +9,21 @@ def stalin_sort(nums):
             continue
         new_nums[pointer] = nums[i]
         pointer += 1
-    return new_nums, pointer
+    return new_nums[0:pointer]
 
 #tests?
 h = [1,2,3,4,5]
-stalined_h, p = stalin_sort(h)
+stalined_h = stalin_sort(h)
 assert stalined_h == h
 
 i = [1,2,4,3,5]
-stalined_i, p = stalin_sort(i)
-assert stalined_i[0:p] == [1,2,4,5]
+stalined_i = stalin_sort(i)
+assert stalined_i == [1,2,4,5]
 
 j = [5,1,2,3,4]
-stalined_j, p = stalin_sort(j)
-assert stalined_j[0:p] == [5]
+stalined_j = stalin_sort(j)
+assert stalined_j == [5]
 
 k = [3,1,2,6,5,9,8]
-stalined_k, p = stalin_sort(k)
-assert stalined_k[0:p] == [3,6,9]
+stalined_k = stalin_sort(k)
+assert stalined_k == [3,6,9]
